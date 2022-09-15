79ceaced1405e5cb72b99b941a71219b78d6b9b5
jtho162 <30760872+jtho162@users.noreply.github.com>
2022-09-15 15:42:52 -0500

    Create gvm-logs.sh

---
+++ gvm-logs.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+# This script is used to quickly identify GVM services, then view the last few log entries. I'm lazy and don't want to check all of these by hand. Sue me
+set -e
+
+DOWNLOAD_DIR=$HOME/greenbone-community-container
+
+echo "Display running services"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition top
+echo
+echo
+
+echo "GSA Logs"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition logs --tail 10 gsa
+wait 5
+echo
+echo
+
+echo "GVMD Logs"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition logs --tail 10 gvmd
+wait 5
+echo
+echo
+
+echo "MQTT Broker Logs"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition logs --tail 10 mqtt-broker
+wait 5
+echo
+echo
+
+echo "Notus Scanner Logs"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition logs --tail 10 notus-scanner
+wait 5
+echo
+echo
+
+echo "OSPD SCANNER Logs"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition logs --tail 10 ospd-scanner
+wait 5
+echo
+echo
+
+echo "Postgres GVM Logs"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition logs --tail 10 pg-gvm
+wait 5
+echo
+echo
+
+echo "Redis Logs"
+docker-compose -f $DOWNLOAD_DIR/docker-compose-$RELEASE.yml -p greenbone-community-edition logs --tail 10 redis-server
+wait 5
+echo
+echo
